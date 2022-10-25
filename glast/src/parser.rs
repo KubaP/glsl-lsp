aa9dc42e50d8266f926d4fbb7a9e8c554a32e376
KubaP <git@kubap.io>
2022-10-25 17:03:40 +0100

    lib: Implement full conditional compilation
    - Replace the existing simple implementation with one that correctly deals with all edge cases when it comes to conditional branching.
    - Change the return type of the main parsing function to return a struct instead, which controls the conditional block toggles.

--- glast/src/parser.rs
+++ glast/src/parser.rs
@@ -4,7 +4,6 @@ mod syntax;
 pub use syntax::*;
 
 use crate::{
-	cst::{Comment, Comments},
 	error::{Diag, PreprocDefineDiag},
 	token::{preprocessor::TokenStream as PreprocStream, Token, TokenStream},
 	Span, Spanned,
@@ -12,122 +11,349 @@ use crate::{
 use ast::*;
 use std::collections::{HashMap, HashSet};
 
-pub fn parse_from_str(
-	source: &str,
-) -> HashMap<Vec<usize>, (Vec<ast::Node>, Vec<Diag>, Vec<Spanned<SyntaxToken>>)>
-{
-	let (token_stream, metadata) = crate::token::parse_from_str(source);
+pub type TranslationUnit = (Vec<ast::Node>, Vec<Diag>, Vec<Spanned<SyntaxToken>>);
 
-	let token_streams = if metadata.contains_conditional_compilation {
-		// Work out conditional nesting.
+pub fn parse_from_str(source: &str) -> TokenTree {
+	let (mut token_stream, metadata) = crate::token::parse_from_str(source);
 
-		// This numbers by order in each group. Replace with // alt: to number by order of appearance irrespective
-		// of nesting.
-		//alt: let mut num = 0;
-		let mut numbers = vec![0];
-		let mut streams = HashMap::new();
-		streams.insert(numbers.clone(), vec![]);
-		for (token, token_span) in token_stream {
-			match token {
-				Token::Directive(d) => match d {
-					PreprocStream::IfDef { .. }
-					| PreprocStream::IfNotDef { .. }
-					| PreprocStream::If { .. } => {
-						//alt: num += 1;
-						let existing = streams.get(&numbers).unwrap();
-						//alt: numbers.push(num);
-						numbers.push(0);
-						streams.insert(numbers.clone(), existing.clone());
-					}
-					PreprocStream::ElseIf { .. }
-					| PreprocStream::Else { .. } => {
-						//alt: num += 1;
-						if numbers.len() == 1 {
-							// TODO: Emit error.
+	// Skip tree generation if there are no conditional compilation blocks.
+	if !metadata.contains_conditional_compilation {
+		return TokenTree {
+			content: vec![TreeNode {
+				parent: None,
+				content: vec![Content::Tokens(token_stream)],
+			}],
+			order_by_appearance: vec![],
+			contains_conditional_compilation: false,
+		};
+	}
+
+	fn push_condition(arena: &mut Vec<TreeNode>, id: usize) -> usize {
+		let new_id = arena.len();
+		arena.push(TreeNode {
+			parent: Some(id),
+			content: vec![Content::Tokens(vec![])],
+		});
+		arena
+			.get_mut(id)
+			.unwrap()
+			.content
+			.push(Content::ConditionalBlock {
+				if_: new_id,
+				completed_if: false,
+				elses: vec![],
+			});
+		new_id
+	}
+
+	fn push_token(arena: &mut Vec<TreeNode>, id: usize, token: Spanned<Token>) {
+		let node = arena.get_mut(id).unwrap();
+		match node.content.last_mut() {
+			Some(content) => match content {
+				Content::Tokens(v) => v.push(token),
+				Content::ConditionalBlock { .. } => {
+					node.content.push(Content::Tokens(vec![token]));
+				}
+			},
+			None => node.content.push(Content::Tokens(vec![token])),
+		};
+	}
+
+	let mut arena = vec![TreeNode {
+		parent: None,
+		content: vec![Content::Tokens(vec![])],
+	}];
+	let mut stack = vec![0];
+	let mut order_by_appearance = vec![(0, 0)];
+	loop {
+		let (token, token_span) = if !token_stream.is_empty() {
+			token_stream.remove(0)
+		} else {
+			break;
+		};
+
+		match token {
+			Token::Directive(d) => match d {
+				PreprocStream::IfDef { .. }
+				| PreprocStream::IfNotDef { .. }
+				| PreprocStream::If { .. } => {
+					let new_id =
+						push_condition(&mut arena, *stack.last().unwrap());
+					order_by_appearance.push((new_id, *stack.last().unwrap()));
+					stack.push(new_id);
+				}
+				PreprocStream::ElseIf { .. } => {
+					if stack.len() > 1 {
+						stack.pop();
+						let new_id = arena.len();
+						arena.push(TreeNode {
+							parent: Some(*stack.last().unwrap()),
+							content: vec![],
+						});
+						let container =
+							arena.get_mut(*stack.last().unwrap()).unwrap();
+						if let Some(Content::ConditionalBlock {
+							if_: _,
+							completed_if,
+							elses,
+						}) = container.content.last_mut()
+						{
+							*completed_if = true;
+							elses.push((Condition::ElseIf, new_id));
+							order_by_appearance
+								.push((new_id, *stack.last().unwrap()));
+							stack.push(new_id);
 						} else {
-							//numbers.pop();
-							//alt: let existing = streams.get(&numbers).unwrap(); delete this \/
-							let existing = streams
-								.get(&numbers[..(numbers.len() - 1)])
-								.unwrap();
-							*numbers.last_mut().unwrap() += 1;
-							//alt: numbers.push(num);
-							streams.insert(numbers.clone(), existing.clone());
+							// TODO: Error.
 						}
+					} else {
+						// TODO: Emit error.
 					}
-					PreprocStream::EndIf { .. } => {
-						if numbers.len() == 1 {
-							// TODO: Emit error.
+				}
+				PreprocStream::Else { .. } => {
+					if stack.len() > 1 {
+						stack.pop();
+						let new_id = arena.len();
+						arena.push(TreeNode {
+							parent: Some(*stack.last().unwrap()),
+							content: vec![],
+						});
+						let container =
+							arena.get_mut(*stack.last().unwrap()).unwrap();
+						if let Some(Content::ConditionalBlock {
+							if_: _,
+							completed_if,
+							elses,
+						}) = container.content.last_mut()
+						{
+							*completed_if = true;
+							elses.push((Condition::Else, new_id));
+							order_by_appearance
+								.push((new_id, *stack.last().unwrap()));
+							stack.push(new_id);
 						} else {
-							numbers.pop();
+							// TODO: Error.
 						}
+					} else {
+						// TODO: Emit error.
 					}
-					_ => {
-						'streams: for (k, v) in streams.iter_mut() {
-							if k == &numbers {
-								v.push((
-									Token::Directive(d.clone()),
-									token_span,
-								));
-								continue;
-							}
+				}
+				PreprocStream::EndIf { .. } => {
+					if stack.len() > 1 {
+						stack.pop();
+					} else {
+						// TODO: Emit error.
+					}
+				}
+				_ => push_token(
+					&mut arena,
+					*stack.last().unwrap(),
+					(Token::Directive(d), token_span),
+				),
+			},
+			_ => push_token(
+				&mut arena,
+				*stack.last().unwrap(),
+				(token, token_span),
+			),
+		}
+	}
 
-							if k.len() >= 2 {
-								for i in (0..(k.len() - 1)).rev() {
-									if &k[..=i] == &numbers {
-										v.push((
-											Token::Directive(d.clone()),
-											token_span,
-										));
-										continue 'streams;
+	TokenTree {
+		content: arena,
+		order_by_appearance,
+		contains_conditional_compilation: true,
+	}
+}
+
+pub struct TokenTree {
+	/// # Invariants
+	/// If `contains_conditional_compilation` is `false`, this is:
+	/// ```
+	/// vec![TreeNode {
+	///		parent: None,
+	///		content: vec![Content::Tokens(token_stream)],
+	///	}]
+	/// ```
+	content: Vec<TreeNode>,
+	/// IDs of the relevant nodes ordered by appearance.
+	///
+	/// - `0` - The ID of the `[n]`th conditional node,
+	/// - `1` - The ID of the node which this conditional node depends on.
+	///
+	/// # Invariants
+	/// If `contains_conditional_compilation` is `false`, this is empty.
+	order_by_appearance: Vec<(usize, usize)>,
+
+	/// Whether there are any conditional compilation brances.
+	contains_conditional_compilation: bool,
+}
+
+impl TokenTree {
+	/// Parses the root token stream.
+	///
+	/// Whilst this is guaranteed to succeed, if the entire source string is wrapped within a conditional block
+	/// this will return an empty AST.
+	pub fn root(&self) -> TranslationUnit {
+		let streams = if !self.contains_conditional_compilation {
+			// Panics: See invariant.
+			match self.content.get(0).unwrap().content.first().unwrap() {
+				Content::Tokens(s) => vec![s.clone()],
+				Content::ConditionalBlock { .. } => unreachable!(),
+			}
+		} else {
+			let mut streams = Vec::new();
+			let node = self.content.get(0).unwrap();
+			for content in &node.content {
+				match content {
+					Content::Tokens(s) => streams.push(s.clone()),
+					Content::ConditionalBlock { .. } => {}
+				}
+			}
+
+			streams
+		};
+
+		let mut walker = Walker::new(streams);
+		let mut nodes = Vec::new();
+		while !walker.is_done() {
+			parse_stmt(&mut walker, &mut nodes);
+		}
+
+		(nodes, walker.diagnostics, walker.syntax_tokens)
+	}
+
+	/// Parses a token tree by enabling conditional branches in the order of their appearance.
+	///
+	/// This method can return an `Err` in the following cases:
+	/// - The `order` has a number which doesn't exist,
+	/// - The `order` has a number which depends on another number that is missing.
+	pub fn parse_by_order_of_appearance(
+		&self,
+		order: impl AsRef<[usize]>,
+	) -> Result<TranslationUnit, ParseErr> {
+		let order = order.as_ref();
+
+		if !self.contains_conditional_compilation {
+			return Err(ParseErr::NoConditionalCompilation);
+		}
+
+		// Check that the order is valid.
+		{
+			let mut visited_node_ids = vec![0];
+			for num in order {
+				let (id, parent_id) = match self.order_by_appearance.get(*num) {
+					Some(t) => t,
+					None => return Err(ParseErr::InvalidNum(*num)),
+				};
+
+				if !visited_node_ids.contains(parent_id) {
+					return Err(ParseErr::InvalidChain(*num));
+				}
+
+				visited_node_ids.push(*id);
+			}
+		}
+
+		let mut order_idx = 0;
+		let mut streams = Vec::new();
+		let mut nodes = vec![(0, 0)];
+		'outer: loop {
+			let current_order_id = {
+				match order.get(order_idx) {
+					Some(num) => match self.order_by_appearance.get(*num) {
+						Some((arena_id, _)) => Some(*arena_id),
+						None => unreachable!(),
+					},
+					None => None,
+				}
+			};
+
+			let (node_id, content_idx) = nodes.last_mut().unwrap();
+			let node = self.content.get(*node_id).unwrap();
+
+			while let Some(inner) = &node.content.get(*content_idx) {
+				*content_idx += 1;
+				match inner {
+					Content::Tokens(s) => streams.push(s.clone()),
+					Content::ConditionalBlock { if_, elses, .. } => {
+						// Check if any of the conditional branches match the current order number.
+						if let Some(current_order_id) = current_order_id {
+							if *if_ == current_order_id {
+								nodes.push((*if_, 0));
+								order_idx += 1;
+								continue 'outer;
+							} else {
+								for (_, else_) in elses {
+									if *else_ == current_order_id {
+										nodes.push((*else_, 0));
+										order_idx += 1;
+										continue 'outer;
 									}
-								}
-							}
-						}
-					}
-				},
-				_ => {
-					'streams: for (k, v) in streams.iter_mut() {
-						if k == &numbers {
-							v.push((token.clone(), token_span));
-							continue;
-						}
-
-						if k.len() >= 2 {
-							for i in (0..(k.len() - 1)).rev() {
-								if &k[..=i] == &numbers {
-									v.push((token.clone(), token_span));
-									continue 'streams;
 								}
 							}
 						}
 					}
 				}
 			}
-		}
 
-		// Invariant: `numbers` will never be empty because all `pop()` calls are guarded by `len > 1`.
-		if numbers.len() != 1 {
-			// TODO: Emit errors for each unclosed conditional block.
-		}
-		streams
-	} else {
-		let mut stream = HashMap::new();
-		stream.insert(vec![0], token_stream);
-		stream
-	};
-
-	token_streams
-		.into_iter()
-		.map(|(k, v)| {
-			let mut walker = Walker::new(v);
-			let mut nodes = Vec::new();
-			while !walker.is_done() {
-				parse_stmt(&mut walker, &mut nodes);
+			// We have consumed all the content of this node which means we can pop it from the stack and continue
+			// with the parent node (if there is one).
+			if nodes.len() > 1 {
+				nodes.pop();
+			} else {
+				break 'outer;
 			}
-			(k, (nodes, walker.diagnostics, walker.syntax_tokens))
-		})
-		.collect()
+		}
+
+		let mut walker = Walker::new(streams);
+		let mut nodes = Vec::new();
+		while !walker.is_done() {
+			parse_stmt(&mut walker, &mut nodes);
+		}
+
+		Ok((nodes, walker.diagnostics, walker.syntax_tokens))
+	}
+
+	#[allow(unused)]
+	pub fn parse_by_order_of_nesting(
+		&self,
+		order: impl AsRef<[(usize, usize)]>,
+	) -> Option<TranslationUnit> {
+		todo!()
+	}
+}
+
+#[derive(Debug)]
+pub enum ParseErr {
+	/// The number doesn't exist.
+	InvalidNum(usize),
+	/// The number has a dependent number that was not specified.
+	InvalidChain(usize),
+	/// This tree contains no conditional compilation branches.
+	NoConditionalCompilation,
+}
+
+#[derive(Debug)]
+struct TreeNode {
+	parent: Option<usize>,
+	content: Vec<Content>,
+}
+
+#[derive(Debug)]
+enum Content {
+	Tokens(Vec<Spanned<Token>>),
+	ConditionalBlock {
+		if_: usize,
+		completed_if: bool,
+		elses: Vec<(Condition, usize)>,
+	},
+}
+
+#[derive(Debug)]
+enum Condition {
+	ElseIf,
+	Else,
 }
 
 pub(crate) struct Walker {
@@ -137,6 +363,8 @@ pub(crate) struct Walker {
 	/// - `1` - The token stream,
 	/// - `2` - The cursor.
 	streams: Vec<(String, TokenStream, usize)>,
+
+	original_sources: Vec<TokenStream>,
 
 	/// The currently defined macros.
 	///
@@ -161,14 +389,17 @@ pub(crate) struct Walker {
 #[allow(unused)]
 impl Walker {
 	/// Constructs a new walker.
-	fn new(token_stream: TokenStream) -> Self {
+	fn new(mut token_streams: Vec<TokenStream>) -> Self {
 		let mut active_macros = HashSet::new();
 		// Invariant: A macro cannot have no name (an empty identifier), so this won't cause any hashing clashes
 		// with valid macros. By using "" we can avoid having a special case for the root source stream.
 		active_macros.insert("".into());
 
+		let first_source = token_streams.remove(0);
+
 		Self {
-			streams: vec![("".into(), token_stream, 0)],
+			streams: vec![("".into(), first_source, 0)],
+			original_sources: token_streams,
 			macros: HashMap::new(),
 			macro_call_site: None,
 			active_macros,
@@ -274,10 +505,29 @@ impl Walker {
 				// We have reached the end of this stream. We close it and re-run the loop on the next stream (if
 				// there is one).
 
-				// Panic: Anytime a stream is added the identifier is inserted into the set.
-				self.active_macros.remove(identifier);
-				self.streams.remove(self.streams.len() - 1);
-				continue;
+				let ident = identifier.clone();
+				if self.streams.len() == 1 {
+					// If we aren't in a macro, that means we've finished the current source stream but there may
+					// be another one (as they can be split up due to conditional compilation).
+
+					if self.original_sources.is_empty() {
+						// We have reached the final end.
+						self.streams.remove(0);
+						break;
+					} else {
+						let mut next_source = self.original_sources.remove(0);
+						let (_, s, c) = self.streams.get_mut(0).unwrap();
+						std::mem::swap(s, &mut next_source);
+						*c = 0;
+						dont_increment = true;
+						continue;
+					}
+				} else {
+					// Panic: Anytime a stream is added the identifier is inserted into the set.
+					self.active_macros.remove(&ident);
+					self.streams.remove(self.streams.len() - 1);
+					continue;
+				}
 			}
 
 			let (token, token_span) = stream.get(*cursor).unwrap();
@@ -352,7 +602,7 @@ impl Walker {
 		}
 	} */
 
-	/// Returns any potential comment tokens until the next non-comment token.
+	/* /// Returns any potential comment tokens until the next non-comment token.
 	fn consume_comments(&mut self) -> Comments {
 		// FIXME: replacement
 		let mut comments = Vec::new();
@@ -369,7 +619,7 @@ impl Walker {
 			self.advance();
 		}
 		comments
-	}
+	} */
 
 	/// Registers a define macro. Note: currently only supports object-like macros.
 	fn register_macro(&mut self, ident: Spanned<String>, tokens: TokenStream) {

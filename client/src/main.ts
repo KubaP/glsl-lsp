32b784a8cedc096fbf8507a5ff4cf52ca85c5cfd
KubaP <git@kubap.io>
2023-01-05 21:31:19 +0000

    client: Minor improvements
    - Refactor the output channels to have a separate channel for tracing and client logging.
    - Add a bunch of proper client logging calls.
    - Add a command to show the version, which should help anyone filing a issue report.

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,3 +1,4 @@
+import { spawnSync } from "child_process";
 import * as path from "path";
 import * as vscode from "vscode";
 import { ExtensionContext } from "vscode";
@@ -7,8 +8,12 @@ import * as commands from "./commands";
 import { Context } from "./context";
 import * as lsp from "./lsp";
 
-// Global context variable.
+// Global extension context.
 let ctx: Context | undefined;
+
+/* WARNING: Ensure that this matches the value in `package.json` */
+export const EXTENSION_VERSION = "0.0.1";
+export let serverVersion = "<server not found>";
 
 export async function activate(context: ExtensionContext) {
 	// For debugging/development purposes, the environment variable `__GLSL_DEBUG_SERVER_PATH` specifies the path
@@ -17,13 +22,20 @@ export async function activate(context: ExtensionContext) {
 	const serverPath =
 		process.env.__GLSL_DEBUG_SERVER_PATH ?? context.asAbsolutePath(path.join("..", "${GLSL_SERVER_PATH}"));
 
+	serverVersion = spawnSync(serverPath, ["--version"], { encoding: "utf-8" })
+		.stdout.slice("glsl-lsp ".length)
+		.trim();
+
 	// Create our custom context object.
 	ctx = await Context.new(context, serverPath);
 
+	ctx.info(`Extension version: ${EXTENSION_VERSION}`);
+
 	// Register commands.
-	ctx.registerCommand("showAst", commands.showAst);
 	ctx.registerCommand("evaluateConditionals", commands.evaluateConditionals);
 	ctx.registerCommand("disableConditionals", commands.disableConditionals);
+	ctx.registerCommand("showAst", commands.showAst);
+	ctx.registerCommand("showVersion", commands.showVersion);
 
 	// Register LSP handlers.
 	ctx.client.onRequest(ConfigurationRequest.method, lsp.configurationRequest);
@@ -32,6 +44,9 @@ export async function activate(context: ExtensionContext) {
 
 	// Start the client.
 	ctx.client.start();
+	ctx.info("Starting language client/server");
+	ctx.info("Using `glsl-lsp` server binary located at:", serverPath);
+	ctx.info(`Server version: ${serverVersion}`);
 }
 
 export function deactivate(): Thenable<void> | undefined {
